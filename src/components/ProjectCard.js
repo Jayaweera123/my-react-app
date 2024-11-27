2c5f06fb4fd446bdc224926242d32d6ae60a8420
Jayaweera123 <155569167+Jayaweera123@users.noreply.github.com>
2024-11-27 15:58:59 +0530

    edit details

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,57 @@
-import { Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Col, Modal, Button } from "react-bootstrap";
+import { FaGithub } from "react-icons/fa";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, githubLink }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const handleShowDetails = () => setShowDetails(true);
+  const handleCloseDetails = () => setShowDetails(false);
+
   return (
-    <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={imgUrl} />
-        <div className="proj-txtx">
+    <Col md={8} lg={6} className="mb-4">
+      <div className="project-card">
+        {/* Project Image */}
+        <img src={imgUrl} alt={title} />
+
+        {/* Project Content */}
+        <div className="project-content">
           <h4>{title}</h4>
-          <span>{description}</span>
+          <p>{description}</p>
+        </div>
+
+        {/* Footer Section */}
+        <div className="project-footer">
+          {/* GitHub Link */}
+          <div className="github-link">
+            <FaGithub />
+            <a href={githubLink} target="_blank" rel="noopener noreferrer">
+              GitHub
+            </a>
+          </div>
+
+          {/* See Details Button */}
+          <Button className="more-details-btn" onClick={handleShowDetails}>
+            See Details
+          </Button>
         </div>
       </div>
+
+      {/* Modal for More Details */}
+      <Modal show={showDetails} onHide={handleCloseDetails} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>{description}</p>
+          {/* Add additional project details here */}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseDetails}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Col>
-  )
-}
+  );
+};
